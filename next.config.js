df20088bc84d939d1ec1223e07497cc92a3de06b
scrp <scrappyjs666@gmail.com>
2023-05-27 20:15:30 +0300

    some changes

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://37.230.195.26:3000/api/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://37.230.195.26:3000/api/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
